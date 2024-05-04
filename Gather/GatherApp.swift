84309089157cbd3f43bb47ee84a4ce3ea52657e3
Roman Timoshkin <rmntim@yandex.ru>
2024-05-04 13:45:10 +0300

    Initial Commit

---
+++ Gather/GatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherApp.swift
+//  Gather
+//
+//  Created by Roman Timoshkin on 04.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
