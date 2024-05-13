95ed7c3be0c7a65cda8609a2e34a1f0622bd8904
Roman Timoshkin <rmntim@yandex.ru>
2024-05-13 18:22:34 +0300

    feat: add event model

---
+++ Gather/Model/Event/EventModel.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  Gather
+//
+//  Created by Roman Timoshkin on 13.05.2024.
+//
+
+import Foundation
+
+struct Event: Identifiable, Hashable {
+    let id: UUID = UUID()
+    let name: String
+    let date: Date
+}
---
+++ Gather/Model/Post/PostModel.swift
@@ -0,0 +1,27 @@
+//
+//  Post.swift
+//  Gather
+//
+//  Created by Roman Timoshkin on 13.05.2024.
+//
+
+import Foundation
+
+struct Post: Identifiable, Hashable {
+    let id: UUID = UUID()
+    let authorName: String
+    let authorImageUrl: String
+    let publicationDate: Date
+    let description: String
+    let likes: Int
+    let event: Event
+}
+
+extension Post {
+    static func mockData() -> [Post] {
+        return [
+            .init(authorName: "Виктор", authorImageUrl: "https://xsgames.co/randomusers/avatar.php?g=male", publicationDate: Date.now, description: "Это было невероятно...", likes: 43, event: Event(name: "Feduk", date: Date.distantPast)),
+            .init(authorName: "Роман", authorImageUrl: "https://xsgames.co/randomusers/avatar.php?g=male", publicationDate: Date.now, description: "Я ебу что тут писать????", likes: 228, event: Event(name: "QUOK Genesis Tour", date: Date.now)),
+        ]
+    }
+}
